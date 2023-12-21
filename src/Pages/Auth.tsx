af1e4c21d833fa663a7174a3c21a75078e9d45d3
GuillaumeM10 <guillaumemengelatte@hotmail.fr>
2023-12-21 03:07:52 +0100

    fix: sign-in tabs changing

--- src/Pages/Auth.tsx
+++ src/Pages/Auth.tsx
@@ -5,18 +5,16 @@ import "./auth.scss";
 
 const Auth = () => {
   const [tabs, setTabs] = useState<boolean>(false);
-  const [slide, setSlide] = useState<boolean>(false);
 
   const changeTabSlide = () => {
     setTabs(!tabs);
-    setSlide(!slide);
   };
 
   return (
     <div className="auth">
       <div className="slide">
         <div className="slider">
-          <div className={slide ? "activeSlide active" : "activeSlide"} />
+          <div className={tabs ? "activeSlide active" : "activeSlide"} />
         </div>
         <button onClick={changeTabSlide}>Connexion</button>
         <button onClick={changeTabSlide}>Inscription</button>
