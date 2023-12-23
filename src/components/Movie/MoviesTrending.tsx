a5bb49fbef94dd7d95b720a414dd08980cdf474a
GuillaumeM10 <guillaumemengelatte@hotmail.fr>
2023-12-23 05:20:20 +0100

    readme, fix loading size home page

--- src/components/Movie/MoviesTrending.tsx
+++ src/components/Movie/MoviesTrending.tsx
@@ -51,7 +51,7 @@ const MoviesTrending = () => {
 
         </div>
             
-      ) : <img src={Loading} alt="loading" />}
+      ) : <img width={50} height={50} src={Loading} alt="loading" />}
     </Fragment>
   );
 };
