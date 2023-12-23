2671c48cc2b0793a678f61733421275be1866505
GuillaumeM10 <guillaumemengelatte@hotmail.fr>
2023-12-23 05:02:47 +0100

    feat: share movie, similars movies, movie videos, home top movies

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,11 @@
+import MoviesTrending from "../components/Movie/MoviesTrending";
 import PopularMovies from "../components/PopularMovies/PopularMovies";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
+      <MoviesTrending />
       <PopularMovies />
     </div>
   );
--- src/types/movie.type.ts
+++ src/types/movie.type.ts
@@ -94,3 +94,20 @@ export interface Credits {
   cast: Cast[];
   crew: Crew[];
 }
+
+
+export interface Video {
+  id: string;
+  iso_639_1: string;
+  iso_3166_1: string;
+  key: string;
+  name: string;
+  site: string;
+  size: number;
+  type: string;
+}
+
+export interface Videos {
+  id: number;
+  results: Video[];
+}
